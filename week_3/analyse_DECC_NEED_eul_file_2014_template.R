e46d331b87e0315193457d740bf567decf9a3225
Ben Anderson <dataknut@icloud.com>
2015-10-14 11:50:56 +0100

    template file for students to use
    loads data etc

---
+++ week_3/analyse_DECC_NEED_eul_file_2014_template.R
@@ -0,0 +1,106 @@
+# Meta ----
+# Script to load & analyse DECC NEED data
+# Data from: http://discover.ukdataservice.ac.uk/catalogue/?sn=7518
+# See also: https://www.gov.uk/government/collections/national-energy-efficiency-data-need-framework
+
+# code by: b.anderson@soton.ak.uk (@dataknut)
+
+### Housekeeping  ------------------------
+
+# clear the workspace
+rm(list=ls())
+
+# where is the default working directory?
+getwd()
+
+# set a variable with the data location
+# You will need to change this
+dpath <- "~/OneDriveBusiness/PG/Southampton/FEEG6025 Data Analysis & Experimental Methods for Engineers/Data"
+# set a variable for where we want the results to go
+# You will need to change this too!
+rpath <- "~/OneDriveBusiness/PG/Southampton/FEEG6025 Data Analysis & Experimental Methods for Engineers/Week 3"
+
+# input file name
+# long form of data (pre-processed)
+# notice that it has a .dta suffix - this is a STATA format data file
+ifile <- "need_eul_may2014_merged_100pc.dta"
+
+### Required packages ----
+# We're going to load a STATA file so we need the 'foreign' package
+#install.packages("foreign") # if needed
+library(foreign)
+
+### Load data  ------------------------
+
+# change working directory to where the data is
+setwd(dpath)
+
+# load the data into a data frame
+# this may take a few seconds - it is quite large!
+needEulf2014DF <- read.dta(ifile)
+
+# change the working directory once loaded in case we want to save out any results
+setwd(rpath)
+
+# how much data do we have?
+dim(needEulf2014DF)
+
+## Q0: Check out the data ----
+# Q0.0: What are the variable names?
+# Hint: use names()
+
+
+# Q0.1: What kind of data do we have and what are the central tendencies?
+# Hint: use summary()
+
+
+# Q0.1a: Do that again for gas & electricity consumption - do you notice anything strange?
+# Why might that have happened?
+# Hint: use summary() for each variable
+
+
+# Q0.2: How many properties do not have 'valid' elec readings per year?
+# Hint: use table(x,y) and the EconsValid variable
+
+
+# What do the codes G, L, M & V mean?
+# Hint: RTFM :-) -> http://bit.ly/1LazmvE -> "Excel workbook explaining variable codes"
+
+# Q0.2a Draw a mosaicplot to visualise this
+# Hint: You will need 4 colours as there are 4 'valid' codes
+
+# Q0.3: How many properties do not have 'valid' gas readings per year? 
+# How many do not have gas at all?
+# Hint: use table(x,y) and the GconsValid variable
+
+# Q0.3a Draw a mosaicplot to visualise this
+# no hint needed except you will need 5 colours this time to include O (off gas)
+
+
+## Q1: Descriptive Analysis over time ----
+# Q1.1: How does elec consumption vary over time?
+# Hint: use a boxplot(x ~y) and don't forget to add axis labels!
+
+# Q1.2: How does gas consumption vary over time?
+
+## Q2: Descriptive Analysis by UK region for 2012 ----
+# Q2.1 is there any difference in domestic electricity consumption by UK region in 2012?
+# Hint: use a boxplot() but select year == 2012 by sub-setting first & don't forget to label axes
+
+# Q2.1 is there any difference in domestic gas consumption by UK region in 2012?
+# Hint: use a boxplot() but select year == 2012 by sub-setting first
+
+## Q3: Descriptive Analysis by energy efficiency band in 2012 ----
+# Q3.1 is there a difference in domestic electricity consumption by EE band in 2012?
+# Hint: have a quick look at the EE_BAND first
+
+# now use boxplot (no hint, you should know how to do this!)
+
+# Q3.1 is there a difference in domestic electricity consumption by EE band in 2012?
+# (no hint, you should know how to do this!)
+
+# Q4: Is there an association between property age and wall structure using 2012 subset? ----
+# Why use the 2012 subset? What would happen if we used all the data?
+
+# Homework! Edit this file to save the graphs so you can use them in a report ----
+# Hint: refer to the playWithDistributions.R code :-)
